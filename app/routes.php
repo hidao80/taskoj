11e8a5dde82709ab973bd23f0816868d74947bb0
hidao80 <8155294+hidao80@users.noreply.github.com>
2021-01-10 18:48:35 +0900

    feat: 各画面へのルーティングを仮決め

--- app/routes.php
+++ app/routes.php
@@ -24,51 +24,56 @@ Route::get( 'login', function()
 Route::post( 'login', "AuthController@login");
 
 Route::group(['before' => 'auth'], function() { 
-    
+    /*
     Route::get( 'hello', function()
         {
-            return View::make( 'hello' );
+            return View::make( 'task/list' );
         } );
-     
+     */
     Route::get( 'logout', "AuthController@logout");
 
     Route::get( 'user/add', [function()
         {
-            return View::make( 'user', ['type' => 'add', 'user_rank' => Auth::user()->user_rank, 'msg' => ""]);
+            return View::make( 'user/add', ['user_rank' => Auth::user()->user_rank, 'msg' => ""]);
         }] );
     Route::post( 'user/add', ['uses' => 'UserController@create', function()
         {
-            return View::make( 'user', ['type' => 'add', 'user_rank' => Auth::user()->user_rank]);
+            return View::make( 'user/add', ['user_rank' => Auth::user()->user_rank]);
         }] );
 
-    Route::get( 'user/update', [function()
-        {
-            return View::make( 'user', ['type' => 'update', 'user_rank' => Auth::user()->user_rank, 'msg' => ""]);
-        }] );
+    Route::get( 'user/show/{id?}', ['uses' => 'UserController@show'] )->where('id', '[a-zA-Z0-9_-]+');
+
+    Route::get( 'user/update/{id?}', ['uses' => 'UserController@show'] )->where('id', '[a-zA-Z0-9_-]+');
     Route::post( 'user/update', ['uses' => 'UserController@update', function()
         {
-            return View::make( 'user', ['type' => 'update', 'user_rank' => Auth::user()->user_rank]);
+            return View::make( 'user/update', ['user_rank' => Auth::user()->user_rank]);
         }] );
 
     Route::get( 'user/delete', [function()
         {
-            return View::make( 'user', ['type' => 'delete', 'user_rank' => Auth::user()->user_rank, 'msg' => ""]);
+            return View::make( 'user/delete', ['user_rank' => Auth::user()->user_rank, 'msg' => ""]);
         }] );
     Route::post( 'user/delete', ['uses' => 'UserController@delete', function()
         {
-            return View::make( 'user', ['type' => 'delete', 'user_rank' => Auth::user()->user_rank]);
+            return View::make( 'user/delete', ['user_rank' => Auth::user()->user_rank]);
         }] );
 
     Route::get( 'task/edit', function()
         {
-            return View::make( 'task_edit', [
+            return View::make( 'task/edit', [
                 'task_id' => null, 
-                'user' => Auth::user(), 
-                'team_info' => array('staff' => User::where('team', Auth::user()->team)->get(), 'staff_id' => []),
+                'user_rank' => Auth::user()->user_rank, 
+                'team' => Auth::user()->team,
+                'team_info' => array(
+                    'user_list' => User::getUserList(), 
+                    'staff_list' => [],
+                    ),
                 'record' => null,
                 ]);
         } );
     Route::get( 'task/edit/{id}', ['uses' => 'TaskController@getId'] )->where('id', '[0-9]+');
+    Route::get( 'task/delete/{id}', ['uses' => 'TaskController@delete'] )->where('id', '[0-9]+');
     Route::post( 'task/edit', ['uses' => 'TaskController@edit'] );
 
+    Route::get( 'task/list', ['uses' => 'ListController@get'] );
 });
